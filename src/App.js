d2a1b22259836441cb3926b5e3f8e37ce8d7ec34
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2020-12-12 19:03:46 +0530

    Food-recipe header added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './App.css'
 import Header from './Components/Header';
 import Recepies from './Components/Recepies'
 
 function App(){
+
+    const [search,setsearch] = useState("Nitesh")
+    const onInputChange = e =>{
+        setsearch(e.target.value)
+    }
+
     return(
         <>
         <div className="App">
-            <h1>Food Recipe App</h1>
-            <Header />
+            <Header search={search} onInputChange={onInputChange} />
             <Recepies />
         </div>
         </>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,25 @@
-import react from 'react';
+import react from "react";
 
-const Header = () => {
-    return(
-        <>
-        <h1>Header</h1>
-        </>
-    )
-}
+const Header = (props) => {
+  return (
+    <>
+      <div className="jumbotron">
+        <h1 className="display-4">
+          <span class="material-icons Food-icon display-4">fastfood</span>Food
+          Recipes
+        </h1>
+        <p className="lead">
+          Want to craft your own creative meat-free meals? Get inspiration from
+          other food lovers, or share your own masterpieces! Transform into a
+          master chef now.
+        </p>
+        <div class="input-group w-50 mx-auto">
+            <input type="text" class="form-control" placeholder="Search your recipe.." value={props.search} onChange={props.onInputChange}/>
+          <button className="btn btn-success">Search Recipe</button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Header
+export default Header;
