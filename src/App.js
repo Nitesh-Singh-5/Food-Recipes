e208bb632a182c9dad2f78eedc6d8b917ecb5652
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2020-12-13 12:53:13 +0530

    Added Recipes from the API

--- src/App.js
+++ src/App.js
@@ -1,40 +1,42 @@
-import React,{useState, useEffect} from 'react'
-import './App.css'
-import Header from './Components/Header';
-import Recepies from './Components/Recepies'
-import Axios from 'axios';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import Recepies from "./Components/Recepies";
+import Axios from "axios";
 
-function App(){
+function App() {
+  const [search, setsearch] = useState("chiken");
+  const [recipes, setRecipe] = useState([]);
 
-    const [search,setsearch] = useState("chiken")
-    const [recipe,setRecipe] = useState([]);
+  const APP_ID = "96ae9512";
+  const APP_KEY = "dab5bcc296fc5998a5baaf7e9647aabe";
 
-    const APP_ID = "96ae9512";
-    const APP_KEY = "dab5bcc296fc5998a5baaf7e9647aabe";
+  useEffect(() => {
+    getRecipe();
+  }, []);
 
-        
-    useEffect(()=>{
-        getRecipe();
-    },[]);
-    
-    const getRecipe = async () =>{
-        const res = await Axios.get(`https://api.edamam.com/search?q=${search}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+  const getRecipe = async () => {
+    const res = await Axios.get(
+      `https://api.edamam.com/search?q=${search}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
 
-        setRecipe(res.data.hits);
-    };
+    setRecipe(res.data.hits);
+  };
 
-    const onInputChange = e =>{
-        setsearch(e.target.value)
-    }
+  const onInputChange = (e) => {
+    setsearch(e.target.value);
+  };
 
-    return(
-        <>
-        <div className="App">
-            <Header search={search} onInputChange={onInputChange} />
-            <Recepies />
+  return (
+    <>
+      <div className="App">
+        <Header search={search} onInputChange={onInputChange} />
+        <div className="container">
+          <Recepies recipes={recipes} />
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/Components/Recepies.js
+++ src/Components/Recepies.js
@@ -1,9 +1,31 @@
 import react from 'react';
 
-const Recepies = () => {
+const Recepies = (props) => {
+    const { recipes } = props;
+
     return(
         <>
-        <h1>Recepies</h1>
+        <div className="row">
+            {
+                recipes.map(recipe => (
+                    <div className="col-md-3">
+                        <div className="card py-2 text-center">
+                            <img src={recipe.recipe.image} className="img-fluid w-50 mx-auto rounded-circle" alt=""/>
+                            <div className="card-body">
+                                <h5>{recipe.recipe.label}</h5>
+                            </div>
+                            <ul className="list-group list-group-flush">
+                                {
+                                    recipe.recipe.ingredientLines.map(ingredient=>(
+                                        <li className="list-group-item">{ingredient}</li>
+                                    ))
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
         </>
     )
 }
