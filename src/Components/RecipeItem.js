9017cf7bd803e7b06c307637b5dd6a2f64c6967c
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2020-12-14 12:15:16 +0530

    recipe adding

--- src/Components/RecipeItem.js
+++ src/Components/RecipeItem.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React from "react";
 
 const RecipeItem = (props) => {
-    const {name, image, ingredientLines} = props;
-    return (
-        <>
-       
-            <div className="card py-2 text-center">
-                <img src={image} className="img-fluid w-50 mx-auto rounded-circle" alt=""/>
-                        <div className="card-body">
-                            <h5>{name}</h5>
-                        </div>
-                        <ul className="list-group list-group-flush">
-                            {
-                                ingredientLines.map(ingredient=>(
-                                    <li className="list-group-item">{ingredient}</li>
-                                ))
-                            }
-                        </ul>
-            </div>
-                    
-        </>
-    )
-}
+  const { name, image, ingredientLines } = props;
+  return (
+    <>
+      <div className="card py-2 text-center">
+        <img
+          src={image}
+          className="img-fluid w-50 mx-auto rounded-circle"
+          alt=""
+        />
+        <div className="card-body">
+          <h5>{name}</h5>
+        </div>
+        <ul className="list-group list-group-flush">
+          {ingredientLines.map((ingredient) => (
+            <li className="list-group-item">{ingredient}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
 
-export default RecipeItem
+export default RecipeItem;
