2bbb8663986cdc12b321ff1e783cb2ac7ea79255
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2020-12-18 22:04:06 +0530

    commit

--- src/Components/RecipeItem.js
+++ src/Components/RecipeItem.js
@@ -4,21 +4,7 @@ const RecipeItem = (props) => {
   const { name, image, ingredientLines } = props;
   return (
     <>
-      <div className="card py-2 text-center">
-        <img
-          src={image}
-          className="img-fluid w-50 mx-auto rounded-circle"
-          alt=""
-        />
-        <div className="card-body">
-          <h5>{name}</h5>
-        </div>
-        <ul className="list-group list-group-flush">
-          {ingredientLines.map((ingredient) => (
-            <li className="list-group-item">{ingredient}</li>
-          ))}
-        </ul>
-      </div>
+      
     </>
   );
 };
